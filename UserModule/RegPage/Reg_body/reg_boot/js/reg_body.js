a62c9f2d09e652c58454113a9027c0f574882557
Sirius <copander@163.com>
2020-09-27 19:34:37 +0800

    gxb第一次提交

---
+++ UserModule/RegPage/Reg_body/reg_boot/js/reg_body.js
@@ -0,0 +1,8 @@
+function code(){
+	account_reg.style.display="none";
+	code_reg.style.display="block";
+}
+function account(){
+	account_reg.style.display="block";
+	code_reg.style.display="none";
+}
