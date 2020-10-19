02d4c54268504ee547f4677c70f0f7d982ab15d2
Sirius <copander@163.com>
2020-10-19 21:00:38 +0800

    事件跟新、页面完善

--- UserModule/Login/js/login.js
+++ UserModule/Login/js/login.js
@@ -1,5 +1,5 @@
 /* 扫码登录和账号登录的动态效果 */
-/*
+/*	1.
 实现效果： 用户点击扫码/账号登录，展示相应登录界面
 	第一步，获取触发事件的两个div容器的控制按钮
 	第二步，遍历给按钮添加单击事件
@@ -26,3 +26,42 @@ for(var btn of btns){// 遍历每个按钮添加事件
 		}
 	}
 }
+
+/*  2.
+	实现效果：鼠标移入样式改变；鼠标移除样式变回来
+		第一步、鼠标移入元素添加指定类名
+		第二步、鼠标移出元素清除指定类名
+*/
+// 1. 查找
+var code_1=document.querySelector(".code_1");
+// console.log(ewm); // 11
+// 2. 绑定
+code_1.onmouseover=function(){
+	// 3. 查找
+	var ewm=document.querySelector(".code_1>img:first-child");
+	var phone=document.querySelector(".code_1>img:last-child");
+	phone.className="phone1";
+	ewm.className="ewm1";
+};
+code_1.onmouseout=function(){
+	// 3. 查找
+	var ewm=document.querySelector(".code_1>img:first-child");
+	var phone=document.querySelector(".code_1>img:last-child");
+	phone.className="phone";
+	ewm.className="ewm";
+};
+// 1. 查找
+// var phone=document.querySelector(".code_1>img:last-child");
+// // console.log(ewm); // 11
+// // 2. 绑定
+// phone.onmouseover=function(){
+// 	// console.log("over");// 11
+// 	// console.log(this.className);// 11
+// 	// 3. 查找
+// 	this.className="phone1";
+// 	console.log(this.className);
+// };
+// phone.onmouseout=function(){
+// 	// 3. 查找
+// 	this.className="phone";
+// };
--- UserModule/Reg/js/reg.js
+++ UserModule/Reg/js/reg.js
@@ -2,6 +2,7 @@ var input=document.querySelector("#login_1 input");
 // console.log(input);// <input >
 input.focus();
 
+<<<<<<< HEAD
 // 1. 查找
 //查找#login_2下的提交信息button
 
@@ -13,3 +14,13 @@ input.focus();
   // 4. 修改
   // 通过定时器实现读秒效果
   
+=======
+// var btn=document.querySelector("#login_1 .btn_1");
+// console.log(btn);
+// console.log(getComputedStyle(btn).borderRadius);
+/*  
+  获得计算后的样式
+    getComputedStyle(元素对象).具体属性
+                      如果是有减号的则有驼峰原则
+*/
+>>>>>>> 事件跟新、页面完善
