8866dd6b9dfd7d7ff34edfc570bc7ddef8428cdc
Copan123456 <copander@163.com>
2020-10-20 08:01:39 +0800

    login_3

--- UserModule/Reg/js/reg.js
+++ UserModule/Reg/js/reg.js
@@ -1,3 +1,15 @@
 var input=document.querySelector("#login_1 input");
 // console.log(input);// <input >
 input.focus();
+
+// 1. 查找
+//查找#login_2下的提交信息button
+
+// 2. 绑定
+
+  // 3. 查找
+  // login_3下的第一个a元素
+  // 的第一个字
+  // 4. 修改
+  // 通过定时器实现读秒效果
+  
